85cb3439c381fe24ca42328e6103fe74489600fa
João Pedro Noronha <pedro.noronha@uft.edu.br>
2025-04-15 19:15:35 -0300

    fix: adiciona nome das colunas

--- src/main/java/com/jpnoronha/cadastrodeninjas/Missoes/MissaoModel.java
+++ src/main/java/com/jpnoronha/cadastrodeninjas/Missoes/MissaoModel.java
@@ -18,7 +18,11 @@ public class MissaoModel {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "nome")
     private String nome;
+
+    @Column(name = "dificuldade")
     private String dificuldade;
 
     @OneToMany(mappedBy = "missao")
