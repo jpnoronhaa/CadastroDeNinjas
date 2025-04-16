5d7f86af74facae0c240fc470f0294017a20b62c
João Pedro Noronha <pedro.noronha@uft.edu.br>
2025-04-16 09:46:30 -0300

    chore: gitingore e adiciona as credenciais

--- src/main/java/com/jpnoronha/cadastrodeninjas/Missoes/MissaoRepository.java
+++ src/main/java/com/jpnoronha/cadastrodeninjas/Missoes/MissaoRepository.java
@@ -1,5 +1,4 @@
 package com.jpnoronha.cadastrodeninjas.Missoes;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MissaoRepository extends JpaRepository<MissaoModel, Long> {
