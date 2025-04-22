11fda4ce60ba622623688b9a2d2be62ab0459ba2
João Pedro Noronha <pedro.noronha@uft.edu.br>
2025-04-22 15:21:01 -0300

    feat: criar mapper e dto de ninja

--- src/main/java/com/jpnoronha/cadastrodeninjas/Ninjas/NinjaDTO.java
+++ src/main/java/com/jpnoronha/cadastrodeninjas/Ninjas/NinjaDTO.java
@@ -1,5 +1,6 @@
 package com.jpnoronha.cadastrodeninjas.Ninjas;
 
+import com.jpnoronha.cadastrodeninjas.Missoes.MissaoModel;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,5 +12,7 @@ public class NinjaDTO {
     private Long id;
     private String nome;
     private String email;
-
+    private String imgUrl;
+    private int idade;
+    private MissaoModel missao;
 }
