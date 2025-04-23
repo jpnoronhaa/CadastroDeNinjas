4e8e8374d0a484163e353f05f5ec19c34d6c4cf4
João Pedro Noronha <pedro.noronha@uft.edu.br>
2025-04-23 10:38:57 -0300

    refactor: adicionar thymeleaf e final na injeção de dependencia

--- src/main/java/com/jpnoronha/cadastrodeninjas/Ninjas/NinjaService.java
+++ src/main/java/com/jpnoronha/cadastrodeninjas/Ninjas/NinjaService.java
@@ -9,8 +9,8 @@ import java.util.Optional;
 @Service
 public class NinjaService {
 
-    private NinjaRepository ninjaRepository;
-    private NinjaMapper ninjaMapper;
+    private final NinjaRepository ninjaRepository;
+    private final NinjaMapper ninjaMapper;
 
     public NinjaService(NinjaRepository ninjaRepository) {
         this.ninjaRepository = ninjaRepository;
